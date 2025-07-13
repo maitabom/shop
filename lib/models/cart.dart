384bc7c9e454228451b40b787dd52a0406bf0a7c
Fábio Valentim <maitabom@gmail.com>
2025-07-13 20:39:37 -0300

    Adiciona a classe Cart e CartItem para gerenciar o carrinho de compras e seus itens

---
+++ lib/models/cart.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_item.dart';
+import 'package:shop/models/product.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    double total = 0.0;
+
+    _items.forEach((key, cartItem) {
+      total += (cartItem.price * cartItem.quantity);
+    });
+
+    return total;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (previous) => CartItem(
+          id: previous.id,
+          productID: previous.productID,
+          name: previous.name,
+          quantity: previous.quantity + 1,
+          price: previous.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          productID: product.id,
+          name: product.name,
+          quantity: 1,
+          price: product.price,
+        ),
+      );
+    }
+
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
+}
---
+++ lib/models/cart_item.dart
@@ -0,0 +1,15 @@
+class CartItem {
+  final String id;
+  final String productID;
+  final String name;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.productID,
+    required this.name,
+    required this.quantity,
+    required this.price,
+  });
+}
