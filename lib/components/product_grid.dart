f427301fcc3b4d680a6070988b0a98839469e8e1
Fábio Valentim <maitabom@gmail.com>
2025-07-12 21:43:53 -0300

    Adiciona o componente ProductGrid para exibir a lista de produtos em um grid

---
+++ lib/components/product_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/provider/product_list.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> listProducts = provider.items;
+
+    return GridView.builder(
+      itemCount: listProducts.length,
+      padding: EdgeInsets.all(10),
+      itemBuilder: (context, index) {
+        return ProductItem(product: listProducts[index]);
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.5,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
--- lib/pages/products_overview.dart
+++ lib/pages/products_overview.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/models/product.dart';
-import 'package:shop/provider/product_list.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> listProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(title: Text('Minha Loja')),
-      body: GridView.builder(
-        itemCount: listProducts.length,
-        padding: EdgeInsets.all(10),
-        itemBuilder: (context, index) {
-          return ProductItem(product: listProducts[index]);
-        },
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
