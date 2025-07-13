a7aa0964e8a49071d7f11c5b1210bcdd49362495
Fábio Valentim <maitabom@gmail.com>
2025-07-12 21:36:36 -0300

    Refatora a estrutura do aplicativo para utilizar ChangeNotifierProvider e gerenciar o estado da lista de produtos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/pages/product_detail.dart';
 import 'package:shop/pages/products_overview.dart';
+import 'package:shop/provider/product_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,24 +14,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        primarySwatch: Colors.purple,
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.purple,
-          secondary: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          primarySwatch: Colors.purple,
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.purple,
+            foregroundColor: Colors.white,
+            elevation: 4,
+          ),
         ),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.purple,
-          foregroundColor: Colors.white,
-          elevation: 4,
-        ),
+        home: ProductsOverviewPage(),
+        debugShowCheckedModeBanner: false,
+        routes: {'/product-detail': (context) => ProductDetailPage()},
       ),
-      home: ProductsOverviewPage(),
-      debugShowCheckedModeBanner: false,
-      routes: {'/product-detail': (context) => ProductDetailPage()},
     );
   }
 }
--- lib/pages/products_overview.dart
+++ lib/pages/products_overview.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/provider/product_list.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  List<Product> listProducts = dummyProducts;
-
-  ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> listProducts = provider.items;
+
     return Scaffold(
       appBar: AppBar(title: Text('Minha Loja')),
       body: GridView.builder(
