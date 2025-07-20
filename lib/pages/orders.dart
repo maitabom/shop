f2d38df13e836610029147d9169afcbbe14aa7ef
Fábio Valentim <maitabom@gmail.com>
2025-07-20 18:48:03 -0300

    Adiciona a página de pedidos com título "Meus Pedidos"

---
+++ lib/pages/orders.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrdersPage extends StatelessWidget {
+  const OrdersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Meus Pedidos')));
+  }
+}
