bafdcfba9d79d3f0fce241012e9dc58e43507b6f
Fábio Valentim <maitabom@gmail.com>
2025-07-19 22:23:34 -0300

    Adiciona exibição de detalhes do produto na página de detalhes, incluindo imagem, preço formatado e descrição

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:shop/models/product.dart';
 
 class ProductDetailPage extends StatelessWidget {
@@ -9,6 +10,37 @@ class ProductDetailPage extends StatelessWidget {
     final Product product =
         ModalRoute.of(context)!.settings.arguments as Product;
 
-    return Scaffold(appBar: AppBar(title: Text(product.name)));
+    return Scaffold(
+      appBar: AppBar(title: Text(product.name)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(product.imageUrl, fit: BoxFit.cover),
+            ),
+            SizedBox(height: 10),
+            Text(
+              NumberFormat.currency(
+                locale: 'pt_BR',
+                symbol: 'R\$',
+              ).format(product.price),
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(product.description, textAlign: TextAlign.center),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
