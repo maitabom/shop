c1bef27a0ed830031b01ccf759332ced8d63414d
Fábio Valentim <maitabom@gmail.com>
2025-07-20 19:01:27 -0300

    Adiciona o componente AppDrawler com navegação para a loja e pedidos

---
+++ lib/components/app_drawler.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppDrawler extends StatelessWidget {
+  const AppDrawler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(title: Text('Bem-vindo!'), automaticallyImplyLeading: false),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Loja'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Pedidos'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/orders');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
