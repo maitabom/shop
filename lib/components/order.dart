b1a1d6e04aa76070eadcb6ccda14bde702a4ef00
Fábio Valentim <maitabom@gmail.com>
2025-07-20 19:31:28 -0300

    Altera OrderWidget para StatefulWidget e adiciona funcionalidade de expansão para exibir detalhes do pedido

--- lib/components/order.dart
+++ lib/components/order.dart
@@ -2,22 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
   const OrderWidget({required this.order, super.key});
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text(
-          NumberFormat.currency(
-            locale: 'pt_BR',
-            symbol: 'R\$',
-          ).format(order.total),
-        ),
-        subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(order.date)),
-        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              NumberFormat.currency(
+                locale: 'pt_BR',
+                symbol: 'R\$',
+              ).format(widget.order.total),
+            ),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: Icon(Icons.expand_more),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: (widget.order.products.length * 25.0) + 15,
+              child: ListView(
+                children: widget.order.products.map((product) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        product.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${product.quantity} x ${NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$').format(product.price)}',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+        ],
       ),
     );
   }
