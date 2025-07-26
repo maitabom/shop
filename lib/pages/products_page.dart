c4e920b2c277bc42028376e3d66f59acae065fc8
Fábio Valentim <maitabom@gmail.com>
2025-07-26 20:10:00 -0300

    Substitui exibição de nome de produto por ProductItem na página de gerenciamento de produtos

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawler.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/provider/product_list.dart';
 
 class ProductsPage extends StatelessWidget {
@@ -17,7 +18,12 @@ class ProductsPage extends StatelessWidget {
         padding: const EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: products.itemsCount,
-          itemBuilder: (context, index) => Text(products.items[index].name),
+          itemBuilder: (context, index) => Column(
+            children: [
+              ProductItem(product: products.items[index]),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
