12ab3e9f0e337f53073f09b49b0aef8b77ce71c6
Fábio Valentim <maitabom@gmail.com>
2025-08-03 18:57:16 -0300

    Adiciona funcionalidade para enviar dados de produtos ao servidor ao adicionar um novo produto

--- lib/provider/product_list.dart
+++ lib/provider/product_list.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
+  final _baseUrl = 'https://shop-valentim-default-rtdb.firebaseio.com/';
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -14,6 +17,16 @@ class ProductList with ChangeNotifier {
   int get itemsCount => _items.length;
 
   void add(Product product) {
+    http.post(
+      Uri.parse('$_baseUrl/products.json'),
+      body: jsonEncode({
+        'name': product.name,
+        'description': product.description,
+        'price': product.price,
+        'imageUrl': product.imageUrl,
+        'favorite': product.favorite,
+      }),
+    );
     _items.add(product);
     notifyListeners();
   }
