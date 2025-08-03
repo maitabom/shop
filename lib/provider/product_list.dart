0d2daf80ffa145531841b7083356792d98a6af64
Fábio Valentim <maitabom@gmail.com>
2025-08-02 21:00:40 -0300

    Adiciona métodos de atualização e salvamento de produtos na lista de produtos

--- lib/provider/product_list.dart
+++ lib/provider/product_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
@@ -14,5 +16,32 @@ class ProductList with ChangeNotifier {
   void add(Product product) {
     _items.add(product);
     notifyListeners();
+  }
+
+  void update(Product product) {
+    var index = _items.indexWhere((p) => p.id == product.id);
+
+    if (index >= 0) {
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  void save(Map<String, Object> data) {
+    bool hasId = data['id'] != null;
+
+    final product = Product(
+      id: hasId ? data['id'] as String : Random().nextDouble().toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: double.tryParse(data['price'] as String) ?? 0.0,
+      imageUrl: data['imageUrl'] as String,
+    );
+
+    if (hasId) {
+      update(product);
+    } else {
+      add(product);
+    }
   }
 }
