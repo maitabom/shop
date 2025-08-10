fa6e0e2aeaa335ba4615b149352b5ed3bb194ac8
Fábio Valentim <maitabom@gmail.com>
2025-08-10 20:20:08 -0300

    Refatora OrdersPage para StatefulWidget e implementa carregamento assíncrono de pedidos

--- lib/pages/orders.dart
+++ lib/pages/orders.dart
@@ -4,8 +4,26 @@ import 'package:shop/components/app_drawler.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/provider/order_list.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({super.key});
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Provider.of<OrderList>(context, listen: false).loadOrders().then((_) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +32,13 @@ class OrdersPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Meus Pedidos')),
       drawer: AppDrawler(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (context, index) =>
-            OrderWidget(order: orders.items[index]),
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: orders.itemsCount,
+              itemBuilder: (context, index) =>
+                  OrderWidget(order: orders.items[index]),
+            ),
     );
   }
 }
--- lib/provider/order_list.dart
+++ lib/provider/order_list.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:shop/config/constants.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/provider/cart.dart';
 import 'package:http/http.dart' as http;
@@ -18,7 +19,38 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
-  Future<void> add(Cart cart) async {
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(Uri.parse('$_baseUrl/orders.json'));
+
+    if (response.body == 'null') return;
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    data.forEach((key, item) {
+      _items.add(
+        Order(
+          id: key,
+          date: DateTime.parse(item['date']),
+          total: item['total'],
+          products: (item['products'] as List<dynamic>).map((cartItem) {
+            return CartItem(
+              id: cartItem['id'],
+              productID: cartItem['productId'],
+              name: cartItem['name'],
+              quantity: cartItem['quantity'],
+              price: cartItem['price'],
+            );
+          }).toList(),
+        ),
+      );
+    });
+
+    notifyListeners();
+  }
+
+  Future<void> add(Cart cart) {
     final currentDate = DateTime.now();
 
     final future = http.post(
