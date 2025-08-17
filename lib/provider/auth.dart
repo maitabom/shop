401cf699211ed1fab58ac1a0fc42475459f56a36
Fábio Valentim <maitabom@gmail.com>
2025-08-17 20:35:54 -0300

    Adiciona gerenciamento de autenticação com armazenamento de token, email e UID na classe Auth

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -9,6 +9,25 @@ class Auth with ChangeNotifier {
   final String _baseUrl = dotenv.env['FIREBASE_BASE_URL'] ?? '';
   final String _key = dotenv.env['FIREBASE_API_KEY'] ?? '';
 
+  String? _token;
+  String? _email;
+  String? _uid;
+  DateTime? _expireDate;
+
+  bool get isAuth {
+    final isValid = _expireDate?.isAfter(DateTime.now()) ?? false;
+
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+
+  String? get email {
+    return isAuth ? _email : null;
+  }
+
   Future<void> signUp(String email, String password) async {
     final String fragment = 'signUp';
 
@@ -41,6 +60,16 @@ class Auth with ChangeNotifier {
 
     if (body['error'] != null) {
       throw AuthException(body['error']['message'] ?? '');
+    } else {
+      _token = body['idToken'];
+      _email = body['email'];
+      _uid = body['localId'];
+
+      _expireDate = DateTime.now().add(
+        Duration(seconds: int.parse(body['expiresIn'])),
+      );
+
+      notifyListeners();
     }
   }
 }
