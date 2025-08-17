3138e8d5e079a955c71d66773f1e763a1c316ce0
Fábio Valentim <maitabom@gmail.com>
2025-08-17 20:14:45 -0300

    Adiciona a classe AuthException para gerenciar erros de autenticação

---
+++ lib/errors/auth_exception.dart
@@ -0,0 +1,20 @@
+class AuthException implements Exception {
+  final String key;
+
+  static const Map<String, String> errors = {
+    'EMAIL_EXISTS': 'Já existe e-mail cadastrado no sistema.',
+    'OPERATION_NOT_ALLOWED': 'Operação não permitida.',
+    'TOO_MANY_ATTEMPTS_TRY_LATER':
+        'Acesso bloqueado temporariamente. Tente mais tarde',
+    'EMAIL_NOT_FOUND': 'E-mail não encontrado.',
+    'INVALID_PASSWORD': 'A senha informada está incorreta.',
+    'USER_DISABLED': 'O usuário encontra-se desabilitado no sistema.',
+  };
+
+  AuthException(this.key);
+
+  @override
+  String toString() {
+    return errors[key] ?? 'Ocorreu um erro ao se autenticar no sistema';
+  }
+}
